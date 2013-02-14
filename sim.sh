a2bdb04fab629f542999a57c1e0c48d456abb9e4
jonathan <jonathan@jonathan-laptop.(none)>
2013-02-14 08:37:24 -0600

    imprime las graficas

--- sim.sh
+++ sim.sh
@@ -1,9 +1,9 @@
-FILENAME='data'
-NTIMES=10
-FREQUENCY='0.3 0.6 0.8'
-LENGTH=30
-PROBZERO=0.7
-PROBONE=0.8
+FILENAME='data'#name of the .dat's to be plot
+NTIMES=10 #number of times that the word will be transmited
+FREQUENCY='0.3 0.6 0.8' #frequencies that a 0 will be added to the new word
+LENGTH=30 #max length of the words
+PROBZERO=0.7 #probability that a 0 will be transmited as a 0
+PROBONE=0.8 #probability that a 1 will be transmited as a 1
 
 for f in $FREQUENCY
 do
@@ -15,4 +15,14 @@ do
 	    echo python simulation.py $FILENAME$f".dat" $NTIMES $f $LENGTH $PROBZERO $PROBONE
 	done
     done
+PLOTFILE="graph"$f".plot" #name of the .plot file 
+>$PLOTFILE #access to the .plot file
+echo set term png >>$PLOTFILE
+echo set xlabel \""Length"\" >>$PLOTFILE
+echo set ylabel \""Average"\">>$PLOTFILE
+echo set output \""chart"$f".png"\">>$PLOTFILE
+echo plot \"$FILENAME$f".dat"\" with lines>>$PLOTFILE
+
+gnuplot *.plot
+rm *.plot
 done
