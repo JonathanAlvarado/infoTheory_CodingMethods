ee9ab9e384899d1480d51880f7949e5203889e38
jonathan <jonathan@jonathan-laptop.(none)>
2013-02-13 19:55:08 -0600

    simulacion completa

--- simulation.py
+++ simulation.py
@@ -1,16 +1,27 @@
-import random,sys
+import random,sys,math
 
 class Simulation:
 
-    def __init__(self,frequency,length,nTimes,prob0, prob1):
+    def __init__(self,frequency,length,nTimes,prob0, prob1, fileName):
         self.freq = frequency
-        self.length = length
-        self.probs = [[prob0, 1-prob0],[1-prob1, prob1]]
-        self.simulator(nTimes)
+        self.probs = ([prob0, 1-prob0],[1-prob1, prob1])
+        self.successes = []
+        self.doc = open(fileName,'w')
+        
+        for i in range(1, (length+1)):
+            self.successes.append(self.simulator(i,nTimes))
+       
+        self.average = self.average(self.successes)
+        self.stdDes = self.standardDesviation(self.successes)
+        #print self.average, "  ",self.stdDes
+        
+        for i in range(1, length+1):
+            print i, ' ', self.average, ' ' , self.stdDes
+            self.doc.write( str(i) +' '+ str(self.average)+ ' ' + str(self.stdDes) +' '+ '\n')
 
-    def wordGenerator(self):
+    def wordGenerator(self, length):
         word = []
-        for i in range(self.length):
+        for i in range(length):
             rand = random.uniform(0,1)
             if rand > self.freq:
                 word.append(0)
@@ -19,6 +30,18 @@ class Simulation:
         #print word
         return word
     
+    def simulator(self,length, nTimes):
+        success = 0
+        for i in range(nTimes):
+            word = self.wordGenerator(length)
+            received = self.transmission(word)
+            
+            equal = self.comparison(word,received)
+            if equal : success +=1
+        #print success
+        average = float(success)/nTimes
+        return average
+
     def transmission(self,word):
         received = []
         for i in range(len(word)):
@@ -36,21 +59,41 @@ class Simulation:
                     received.append(1)
         #print received
         return received
-        
-    def simulator(self, nTimes):
-        for i in range(nTimes):
-            word = self.wordGenerator()
-            received = self.transmission(word)
 
+    def comparison(self,word,received):
+        wrd = ''
+        rcv = ''
+        for i in range(len(word)):
+            wrd += str(word[i])
+            rcv += str(received[i])
+        #print wrd, "   ", rcv
+        if wrd == rcv : return True
+        else : return False
+
+    def average(self, successes):
+        average = 0
+        for i in successes:
+            average +=1
+        average = average/len(successes)
+        return average
+
+    def standardDesviation(self,successes):
+        variance = 0.0
+        for i in successes:
+            variance = pow( (i - self.average), 2)
+        variance = variance / len(successes)
+        stdDes = pow(variance,0.5)
+        return stdDes
 
 def main():
-    nTimes = int(sys.argv[1])
-    frequency = float(sys.argv[2])
-    length = int(sys.argv[3])
-    prob0 = float(sys.argv[4])
-    prob1 = float(sys.argv[5])
+    fileName = sys.argv[1]
+    nTimes = int(sys.argv[2])
+    frequency = float(sys.argv[3])
+    length = int(sys.argv[4])
+    prob0 = float(sys.argv[5])
+    prob1 = float(sys.argv[6])
     
-    sim = Simulation(frequency, length, nTimes,prob0,prob1)
+    sim = Simulation(frequency, length, nTimes,prob0,prob1, fileName)
 
 if __name__ == "__main__":
     main()
